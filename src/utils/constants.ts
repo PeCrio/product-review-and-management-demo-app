6b8e8848e0c333d42cd154e670077b5686fff7e7
Precious OSSAI <theossaiprecious@gmail.com>
2022-05-02 18:48:46 +0100

    chore: add constants for urls

---
+++ src/utils/constants.ts
@@ -0,0 +1,21 @@
+export const AUTHOR_WEBSITE_URL = "https://ossaiprecious.com";
+export const AUTHOR_GITHUB_URL = "https://github.com/PeCrio";
+export const PROJECT_DOCS_URL =
+	"https://github.com/PeCrio/product-review-and-management-demo-app";
+export const AUTHOR_EMAIL_URL = "mailto:theossaiprecious@gmail.com";
+export const AUTHOR_LINKEDIN_URL = "https://linkedin.com/in/ossaiprecious";
+
+export const developerDetails = [
+	{
+		title: "GitHub",
+		url: AUTHOR_GITHUB_URL,
+	},
+	{
+		title: "LinkedIn",
+		url: AUTHOR_LINKEDIN_URL,
+	},
+	{
+		title: "Website",
+		url: AUTHOR_WEBSITE_URL,
+	},
+];
