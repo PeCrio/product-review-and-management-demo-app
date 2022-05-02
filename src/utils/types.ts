f2c1f8764615fe60bbc742b6e3060fd13bccae02
Precious OSSAI <theossaiprecious@gmail.com>
2022-05-02 17:18:41 +0100

    chore: add typescript types for reviews

---
+++ src/utils/types.ts
@@ -0,0 +1,9 @@
+export type ReviewRatingI = 1 | 2 | 3 | 4 | 5;
+export interface ReviewI {
+	firstname: string;
+	lastname: string;
+	email: string;
+	rating: ReviewRatingI;
+	comment: string;
+	id?: string;
+}
