47f96debbe10e694ed8263e86b622c3128098e52
Precious OSSAI <theossaiprecious@gmail.com>
2022-07-07 00:50:48 +0100

    refactor: use different api for retrieving reviews

--- src/services/ReviewServices.ts
+++ src/services/ReviewServices.ts
@@ -1,11 +1,31 @@
 import axios from "axios";
-import { ReviewI } from "utils/types";
+import { ReviewI, ReviewRatingI } from "utils/types";
+
+type Rating = {
+	user: string;
+	email: string;
+	comment: string;
+	rating: ReviewRatingI;
+	date: Date;
+};
+
+type RatingResponse = {
+	ratings: Rating[];
+};
 
 export const createReview = async (values: ReviewI) => {
 	const res = await axios.post("/reviews", values);
 	return res.data;
 };
 export const getAllReviews = async () => {
-	const res = await axios.get("/reviews");
-	return res.data;
+	const res = await axios.get<RatingResponse>(
+		"https://random-ratings.vercel.app/api/ratings"
+	);
+	return {
+		data: res.data.ratings.map(({ user, date, ...params }) => ({
+			...params,
+			firstname: user,
+			lastname: "",
+		})),
+	};
 };
