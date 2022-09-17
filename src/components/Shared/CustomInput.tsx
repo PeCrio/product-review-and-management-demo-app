cbe8b28acf19531f42efa4de7ebc2f9ad0645413
Precious OSSAI <theossaiprecious@gmail.com>
2022-09-17 22:08:50 +0100

    feat: type updates

--- src/components/Shared/CustomInput.tsx
+++ src/components/Shared/CustomInput.tsx
@@ -1,12 +1,12 @@
 import {
-	FormControl,
-	FormErrorMessage,
-	FormLabel,
-	Input,
-	InputProps,
-	Textarea,
-	TextareaProps,
-	Box,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  InputProps,
+  Textarea,
+  TextareaProps,
+  Box,
 } from "@chakra-ui/react";
 import { Field, FieldProps as FormikFieldProps } from "formik";
 import React from "react";
@@ -14,66 +14,66 @@ import React from "react";
 type FieldType = "text" | "textarea";
 type CustomInputFieldProps = TextareaProps | InputProps;
 
-interface CustomInputProps {
-	label: string;
-	name: string;
-	fieldProps?: CustomInputFieldProps;
-	type?: FieldType;
+interface Props {
+  label: string;
+  name: string;
+  fieldProps?: CustomInputFieldProps;
+  type?: FieldType;
 }
-function CustomInput({ fieldProps, label, name, type }: CustomInputProps) {
-	const pickFormField = ({
-		formikProps,
-	}: {
-		formikProps: FormikFieldProps | any;
-	}) => {
-		const { field } = formikProps;
-		switch (type) {
-			case "textarea":
-				return (
-					<Textarea
-						id={name}
-						name={name}
-						bg="white"
-						{...field}
-						{...fieldProps}
-					></Textarea>
-				);
+const CustomInput: React.FC<Props> = ({ label, name, fieldProps, type }) => {
+  const pickFormField = ({
+    formikProps,
+  }: {
+    formikProps: FormikFieldProps | any;
+  }) => {
+    const { field } = formikProps;
+    switch (type) {
+      case "textarea":
+        return (
+          <Textarea
+            id={name}
+            name={name}
+            bg="white"
+            {...field}
+            {...fieldProps}
+          ></Textarea>
+        );
 
-			default:
-				return (
-					<Input id={name} name={name} bg="white" {...field} {...fieldProps} />
-				);
-		}
-	};
-	return (
-		<>
-			<Field name={name}>
-				{(formikProps: FormikFieldProps) => {
-					const { form } = formikProps;
-					const isInvalid = !!(form.errors[name] && form.touched[name]);
-					return (
-						<FormControl isInvalid={isInvalid}>
-							<FormLabel mb="1" fontSize="sm" color="gray.500">
-								{label}
-								{fieldProps?.isRequired && (
-									<Box as="span" color="red">
-										*
-									</Box>
-								)}
-							</FormLabel>
-							{pickFormField({
-								formikProps,
-							})}
+      default:
+        return (
+          <Input id={name} name={name} bg="white" {...field} {...fieldProps} />
+        );
+    }
+  };
+  return (
+    <>
+      <Field name={name}>
+        {(formikProps: FormikFieldProps) => {
+          const { form } = formikProps;
+          const isInvalid = !!(form.errors[name] && form.touched[name]);
+          return (
+            <FormControl isInvalid={isInvalid}>
+              <FormLabel mb="1" fontSize="sm" color="gray.500">
+                {label}
+                {fieldProps?.isRequired && (
+                  <Box as="span" color="red">
+                    *
+                  </Box>
+                )}
+              </FormLabel>
+              {pickFormField({
+                formikProps,
+              })}
 
-							<FormErrorMessage fontSize="xs" lineHeight="none">
-								{form.errors[name]}
-							</FormErrorMessage>
-						</FormControl>
-					);
-				}}
-			</Field>
-		</>
-	);
-}
+              <FormErrorMessage fontSize="xs" lineHeight="none">
+                {form.errors[name]}
+              </FormErrorMessage>
+            </FormControl>
+          );
+        }}
+      </Field>
+    </>
+  );
+};
 
 export default CustomInput;
